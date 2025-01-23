4eeed2cb2b9fe597922f5c4ff2c1be57333896b5
Izaias Lima <izaiaslima356@gmail.com>
2025-01-23 01:25:13 -0300

    fix: erro de importação do arquivo routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
-import { AppRoutes } from "./routes/routes.tsx";
+import { AppRoutes } from "./routes/routes";
 
 import "./index.css";
 
